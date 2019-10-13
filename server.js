02005195b1eeebd7feaa6a80497f63fa7377aebf
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-10-12 19:03:13 -0600

    Added new stylesheet and form with bootstrap

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 // Sets up the Express App
@@ -9,12 +10,14 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
